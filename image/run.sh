a81ce664f0e6a6c0ac9156945a1baa27f51cbace
Kristian Jones <kristianjonesg6@gmail.com>
2017-10-01 13:34:32 -0500

    Update run.sh

--- image/run.sh
+++ image/run.sh
@@ -6,4 +6,4 @@ npm install
 # Start CastWebAPI
 ipaddr=$(ip route get 1 | awk '{print $NF;exit}')
 #node /castwebapi/cast-web-api/castWebApi.js --hostname=$ipaddr
-node /xboxon/cast-web-api/castWebApi.js
+node /xboxon/cast-web-api/castWebApi.js --hostname=$ipaddr
