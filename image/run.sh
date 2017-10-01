9ca45e54556180b2c88d9a60ed4a3f01fc27dda9
Kristian Jones <kristianjonesg6@gmail.com>
2017-10-01 13:25:43 -0500

    Update run.sh

--- image/run.sh
+++ image/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd smartthings-xbox
+cd cast-web-api
 npm install
 
 # Start CastWebAPI
 ipaddr=$(ip route get 1 | awk '{print $NF;exit}')
 #node /castwebapi/cast-web-api/castWebApi.js --hostname=$ipaddr
-node /xboxon/smartthings-xbox/app.js
+node /xboxon/cast-web-api/castWebApi.js
